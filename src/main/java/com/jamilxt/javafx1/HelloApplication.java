fe0256ae623ed34f28080d45fd2f33ce5e267c7b
Jamilur Rahman <jamilur.rahman@brainstation-23.com>
2022-10-15 22:42:15 +0600

    add how stage works

--- src/main/java/com/jamilxt/javafx1/HelloApplication.java
+++ src/main/java/com/jamilxt/javafx1/HelloApplication.java
@@ -1,23 +1,39 @@
 package com.jamilxt.javafx1;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image(HelloApplication.class.getResource("jamilxt-avatar.jpg").getPath());
+        stage.getIcons().add(icon);
+
+        stage.setTitle("Stage Demo Program w00t w00t");
+        stage.setScene(scene);
+        stage.setWidth(420);
+        stage.setHeight(420);
+        stage.setResizable(false);
+//        stage.setX(50);
+//        stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.show();
     }
 }

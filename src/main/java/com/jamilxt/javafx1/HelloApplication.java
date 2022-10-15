1840b9efacd56c4d09bf114164f42feb5b20754e
jamilxt <jamilxt@gmail.com>
2022-10-16 01:17:19 +0600

    install gluon scene builder:

--- src/main/java/com/jamilxt/javafx1/HelloApplication.java
+++ src/main/java/com/jamilxt/javafx1/HelloApplication.java
@@ -1,86 +1,24 @@
 package com.jamilxt.javafx1;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Group root = new Group();
-        Scene scene = new Scene(root, 600, 600, Color.LIGHTSKYBLUE);
-        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("sample-1.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        Text text = new Text();
-        text.setText("WHOOOOOOA!");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.LIMEGREEN);
-
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.RED);
-        line.setOpacity(0.5);
-        line.setRotate(45);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(100);
-        rectangle.setWidth(100);
-        rectangle.setHeight(100);
-        rectangle.setFill(Color.BLUE);
-        rectangle.setStrokeWidth(5);
-        rectangle.setStroke(Color.BLACK);
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(
-                200.0, 200.0,
-                300.0, 300.0,
-                200.0, 300.0
-        );
-        triangle.setFill(Color.YELLOW);
-
-        Circle circle = new Circle();
-        circle.setCenterX(350);
-        circle.setCenterY(350);
-        circle.setRadius(50);
-        circle.setFill(Color.ORANGE);
-
-        // TODO: fix the show showing issue
-        Image image = new Image(HelloApplication.class.getResource("jamilxt-avatar.jpg").getPath());
-        ImageView imageView = new ImageView(image);
-        imageView.setX(400);
-        imageView.setY(400);
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(rectangle);
-        root.getChildren().add(triangle);
-        root.getChildren().add(circle);
-        root.getChildren().add(imageView);
-
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }

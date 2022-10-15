a064e6ab2ca3db531ca144cf45e6549a6287c678
jamilxt <jamilxt@gmail.com>
2022-10-16 01:17:44 +0600

    add TODO

--- src/main/java/com/jamilxt/javafx1/HelloApplication.java
+++ src/main/java/com/jamilxt/javafx1/HelloApplication.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
+        // TODO: fix the template loading issue
         Parent root = FXMLLoader.load(HelloApplication.class.getResource("sample-1.fxml"));
         primaryStage.setTitle("Hello World");
         Scene scene = new Scene(root, 300, 275);
